81141f4412a4be049f8d88a00eae93c70c12a71c
Justin Cho <justmyung@gmail.com>
2015-08-24 21:25:20 -0700

    fixed registration bugs, added client side restriction on submit if forms not filled properly

--- public/js/rektmain.js
+++ public/js/rektmain.js
@@ -1,22 +1,107 @@
-$(document).ready(function() {
-    $('#submit').click(function(event){
+// jQuery.validator.addMethod('passwordMatch', function(value, element) {
 
-        data = $('#password').val();
-        var len = data.length;
+//     // The two password inputs
+//     var password = $("#password").val();
+//     var confirmPassword = $("#password_confirm").val();
 
-        if(len < 1) {
-            alert("Password cannot be blank");
-            // Prevent form submission
-            event.preventDefault();
+//     // Check for equality with the password inputs
+//     if (password != confirmPassword ) {
+//         return false;
+//     } else {
+//         return true;
+//     }
+
+// }, "Your Passwords Must Match");
+
+// // ==========================================================================
+// // Registration Form : jquery validation
+// // http://jqueryvalidation.org/validate
+
+// $('#register-form').validate({
+
+//     // rules
+//     rules: {
+//         password: {
+//             required: true,
+//             minlength: 3
+//         },
+//         password_confirm: {
+//             required: true,
+//             minlength: 3,
+//             passwordMatch: true // set this on the field you're trying to match
+//         }
+//     },
+
+//     // messages
+//     messages: {
+//         password: {
+//             required: "What is your password?",
+//             minlength: "Your password must contain more than 3 characters"
+//         },
+//         password_confirm: {
+//             required: "You must confirm your password",
+//             minlength: "Your password must contain more than 3 characters",
+//             passwordMatch: "Your Passwords Must Match" // custom message for mismatched passwords
+//         }
+//     }
+// });//end validate
+
+
+
+(function($,W,D)
+{
+    var JQUERY4U = {};
+
+    JQUERY4U.UTIL =
+    {
+        setupFormValidation: function()
+        {
+            //form validation rules
+            $("#register-form").validate({
+                rules: {
+                    name: "required",
+                    username: "required",
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    password: {
+                        required: true,
+                        minlength: 6
+                    },
+                    password_confirm: {
+                        required: true,
+                        minlength: 5,
+                        equalTo: "#password"
+                    },
+                    agree: "required"
+                },
+                messages: {
+                    name: "Please enter your firstname",
+                    username: "Please enter your lastname",
+                    password: {
+                        required: "Please provide a password",
+                        minlength: "Your password must be at least 5 characters long"
+                    },
+                    email: "Please enter a valid email address",
+                    agree: "Please accept our policy"
+                },
+                submitHandler: function(form) {
+                    form.submit();
+                }
+            });
         }
-         
-        if($('#password').val() != $('#password_confirm').val()) {
-            alert("Password and Confirm Password don't match");
-            // Prevent form submission
-            event.preventDefault();
-        }
-         
+    }
+
+    //when the dom has loaded setup form validation rules
+    $(D).ready(function($) {
+        JQUERY4U.UTIL.setupFormValidation();
     });
+
+})(jQuery, window, document);
+
+jQuery.extend(jQuery.validator.messages, {
+    equalTo: "Passwords do not match!"
 });
 
 
