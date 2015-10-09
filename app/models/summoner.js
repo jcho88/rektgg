386d1476c94bc7456e89d0a4f4248e24bcb5396a
Steve Chen <steveyg88@outlook.com>
2015-10-09 16:29:46 -0700

    half way refresh

--- app/models/summoner.js
+++ app/models/summoner.js
@@ -171,6 +171,7 @@ var SummonerSchema = new Schema({
 		  	}]
   },
 
+  reFreshDate: {type : Date, default : Date.now},
   createdAt  : {type : Date, default : Date.now}
 
 });
@@ -238,11 +239,15 @@ SummonerSchema.statics = {
   
   searchSummByID: function (summonerID, cb) {
 
-      this.findOne({nameNoWhiteSpace : summonerName }, function(err, summoner) {
-        if (!summoner || err) return cb(err, null);
-        loadData(summoner, cb);
-      })   
-    }
+      this.findOne({id: summonerID})
+      .exec(cb);   
+  },
+
+  refresh: function (summonerID, cb) {
+
+      this.findOne({id: id})
+      .exec(cb);   
+  }
 
   
 
