0f429636dfa68578bd77c9a1a5c420ea8a75240b
Justin Cho <jcho088@yahoo.com>
2015-10-26 14:04:44 -0700

    passed in username and id to friend controller, change friend button for users not logged in

--- locales/riot_api.js
+++ locales/riot_api.js
@@ -10,7 +10,7 @@
 	"RANKED_TEAM_5x5": "Ranked Team 5v5",
 	"NORMAL_3x3": "Normal 3v3",
 	"BOT_3x3": "Bot 3v3",
-	"CAP_5x5": "Cap 5v3",
+	"CAP_5x5": "Team Builder",
 	"ARAM_UNRANKED_5x5": "ARAM",
 	"ONEFORALL_5x5": "One For All 5v5",
 	"FIRSTBLOOD_1x1": "First Blood 1v1",
